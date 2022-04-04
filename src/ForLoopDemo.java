697ff03389c79b54663be6cad6bc9d4662b16734
Surwase Vijaya <surwasev0814@gmail.com>
2022-04-05 02:07:40 +0530

    Writing a pattern output into file

--- src/ForLoopDemo.java
+++ src/ForLoopDemo.java
@@ -1,22 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 //Pattern program by using for loop.
 //Program to print the * in 5 rows by decrementing the *
 
 public class ForLoopDemo {
+	public static void writeDataIntoFile(String data,String file) throws IOException{
+		FileWriter writer=new FileWriter(file);
+		BufferedWriter bufferedWriter=new BufferedWriter(writer);
+		bufferedWriter.write(data);
+		bufferedWriter.close();
+	} 
 
-	public void printPatternUsingForLoop() {
+	public void printPatternUsingForLoop() throws IOException{
 		int row = 5;
 		if (row <= 0) {
 			throw new IllegalArgumentException("Row value should be greater than 0");
 		}
+		String fileName="output.txt";
+		String currentRow="";
 		for (; row > 0; row--) {
 			for (int column = 0; column < row; column++) {
-				System.out.print("*");
+				//System.out.print("*");
+				currentRow+="*";
 			}
-			System.out.println("\n");
+			//System.out.println("\n");
+			currentRow +="\n";
 		}
+		writeDataIntoFile(currentRow,fileName);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ForLoopDemo f = new ForLoopDemo();
 		f.printPatternUsingForLoop();
 
